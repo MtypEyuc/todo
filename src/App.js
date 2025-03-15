221ac03467571b9c2525ea89b7eedb368fb974c8
dud <Heo250113@gmail.com>
2025-03-16 06:34:38 +0900

    feat: todo 체크박스 마무리 기능 추가 (2025-03-16) - 기간: 2025-03-16 - 내용: 따라하며 배우는 리액트 A-Z[19버전 반영] 마무리 된 일 표시하기(조건부 삼항 연산자)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends  Component{
             {
                 id : "1",
                 title : "공부하기",
-                completed: true
+                completed: false
             },
             {
                 id : "2",
@@ -26,11 +26,11 @@ export default class App extends  Component{
         float: "right"
     }
 
-    getStyle = () => {
+    getStyle = (completed) => {
         return{
             padding: "10px",
             borderBottom: "1px #ccc dotted",
-            textDecoration: "none"
+            textDecoration: completed ? "line-through" : "none"
         }
     }
 
@@ -55,6 +55,16 @@ export default class App extends  Component{
         this.setState({todoData: [...this.state.todoData, newTodo], value:""});
     }
 
+    handleCompleteChange= (id) => {
+        let newTodoData = this.state.todoData.map((data) => {
+            if(data.id === id){
+                data.completed = !data.completed;
+            }
+            return data;
+        });
+        this.setState({todoData: newTodoData})
+    }
+
   render() {
     return(
         <div className="container">
@@ -62,8 +72,11 @@ export default class App extends  Component{
                 <h1>할 일 목록</h1>
             </div>
             {this.state.todoData.map(data => (
-                <div style={this.getStyle()} key = {data.id}>
-                        <input type="checkbox" defaultChecked={false}/>
+                <div style={this.getStyle(data.completed)} key = {data.id}>
+                        <input
+                        type="checkbox"
+                        onChange={() => {this.handleCompleteChange(data.id)}}
+                        defaultChecked={false} />
                         {data.title}
                         <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
                 </div>
